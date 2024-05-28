e69d1fabe9c8d1573fbf507bca3ee53a69cff6f0
dcpais <dpaismail@gmail.com>
2024-05-29 01:19:26 +1000

    Room rendering working, scaling working properly!

--- Assets/Scripts/RoomSettings.cs
+++ Assets/Scripts/RoomSettings.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RoomSettings : MonoBehaviour
 {
+    // Members
+    [SerializeField]
+    private Transform _transform;
+    [SerializeField]
+    private BoxCollider2D _collider;
+    [SerializeField]
+    private MeshRenderer _renderer;
+    [SerializeField]
+    private MeshFilter _meshFilter;
+    [SerializeField]
+    private LineRenderer _outlineRender;
+
+    // Room Settings
+    [SerializeField]
+    [Range(1.0f, 10.0f)]
+    private float _roomLength;
+    [SerializeField]
+    [Range(1.0f, 10.0f)]
+    private float _roomHeight;
+    
+    // Variables
+    private Vector3[] _outlineVertices;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Set Room Defaults;
+        _roomHeight = 1;
+        _roomLength = 1;
+
+        // Set up outline renderer
+        _outlineRender.positionCount = 4;
+        _outlineRender.loop = true;
+        _outlineRender.startColor = new Color(1, 1, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Update Room Size
+        print(_roomHeight + " " + _roomLength);
+        transform.localScale = new Vector3(_roomLength, _roomHeight, 0);
+
+        // Draw outline
+        _outlineVertices = getOutlinePoints();
+        _outlineRender.SetPositions(_outlineVertices);
+    }
+
+    private Vector3[] getOutlinePoints() {
+        Vector3[] vertices = new Vector3[] {
+            new Vector3(0.5f, 0.5f, -1.0f),
+            new Vector3(0.5f, -0.5f, -1.0f),
+            new Vector3(-0.5f, -0.5f, -1.0f),
+            new Vector3(-0.5f, 0.5f, -1.0f)
+        };
+
+        for (int i = 0; i < vertices.Length; i++) {
+            vertices[i].x *= _roomLength;
+            vertices[i].y *= _roomHeight;
+        }
+
+        return vertices;
     }
 }
